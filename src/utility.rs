9653f8f4a56479a4ac3aa1cc45f421be403532e3
chen-qingyu <236521827@qq.com>
2024-03-09 20:26:30 +0800

    add inline for utility

--- src/utility.rs
+++ src/utility.rs
@@ -8,12 +8,14 @@ pub fn check_bounds(pos: i32, begin: i32, end: i32) {
     }
 }
 
+#[inline]
 pub fn check_empty(size: i32) {
     if size == 0 {
         panic!("Error: The container is empty.");
     }
 }
 
+#[inline]
 pub fn check_full(size: i32, capacity: i32) {
     if size >= capacity {
         panic!(
@@ -23,6 +25,7 @@ pub fn check_full(size: i32, capacity: i32) {
     }
 }
 
+#[inline]
 pub fn calc_index(mut index: i32, size: usize) -> usize {
     if index < 0 {
         index += size as i32;
