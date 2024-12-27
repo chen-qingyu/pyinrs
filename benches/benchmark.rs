2b8edd4f5332773998f95988468341df3348d33a
chen-qingyu <236521827@qq.com>
2024-12-27 16:43:55 +0800

    🧪 bench: Int very fast!

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -22,52 +22,52 @@ criterion_main!(benches);
 /*
 Result (2024.12.27, Windows 10, rustc v1.80.0, criterion v0.5):
 
-Int from                time:   [109.63 ns 110.17 ns 110.88 ns]
-                        change: [+27.543% +32.898% +39.405%] (p = 0.00 < 0.05)
-                        Performance has regressed.
-Found 19 outliers among 100 measurements (19.00%)
-  3 (3.00%) high mild
-  16 (16.00%) high severe
-
-Int +                   time:   [172.22 ns 172.46 ns 172.70 ns]
-                        change: [-7.1230% -6.7329% -6.3369%] (p = 0.00 < 0.05)
-                        Performance has improved.
-Found 5 outliers among 100 measurements (5.00%)
-  1 (1.00%) high mild
-  4 (4.00%) high severe
-
-Int -                   time:   [241.15 ns 241.98 ns 243.46 ns]
-                        change: [-1.3173% -0.7494% -0.1750%] (p = 0.01 < 0.05)
-                        Change within noise threshold.
-Found 9 outliers among 100 measurements (9.00%)
-  3 (3.00%) high mild
-  6 (6.00%) high severe
-
-Int *                   time:   [252.65 ns 252.94 ns 253.30 ns]
-                        change: [-91.106% -91.063% -91.007%] (p = 0.00 < 0.05)
-                        Performance has improved.
-Found 8 outliers among 100 measurements (8.00%)
-  2 (2.00%) high mild
-  6 (6.00%) high severe
-
-Int /                   time:   [715.18 ns 718.97 ns 726.30 ns]
-                        change: [-61.460% -61.194% -60.914%] (p = 0.00 < 0.05)
-                        Performance has improved.
-Found 16 outliers among 100 measurements (16.00%)
-  5 (5.00%) high mild
-  11 (11.00%) high severe
-
-Int %                   time:   [739.69 ns 751.35 ns 766.43 ns]
-                        change: [-61.581% -60.479% -59.393%] (p = 0.00 < 0.05)
-                        Performance has improved.
+Int from                time:   [118.00 ns 118.18 ns 118.37 ns]
+                        change: [-3.1437% +0.9289% +4.5842%] (p = 0.66 > 0.05)
+                        No change in performance detected.
 Found 7 outliers among 100 measurements (7.00%)
-  2 (2.00%) high mild
+  4 (4.00%) high mild
+  3 (3.00%) high severe
+
+Int +                   time:   [152.56 ns 152.94 ns 153.41 ns]
+                        change: [-11.453% -10.533% -8.9234%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 12 outliers among 100 measurements (12.00%)
+  7 (7.00%) high mild
   5 (5.00%) high severe
 
-Int gcd                 time:   [28.489 µs 28.533 µs 28.583 µs]
-                        change: [-31.391% -30.884% -30.395%] (p = 0.00 < 0.05)
+Int -                   time:   [214.18 ns 214.64 ns 215.17 ns]
+                        change: [-11.621% -11.167% -10.724%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 12 outliers among 100 measurements (12.00%)
+  7 (7.00%) high mild
+  5 (5.00%) high severe
+
+Int *                   time:   [156.24 ns 156.57 ns 156.95 ns]
+                        change: [-38.594% -38.191% -37.776%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 15 outliers among 100 measurements (15.00%)
+  8 (8.00%) high mild
+  7 (7.00%) high severe
+
+Int /                   time:   [635.94 ns 637.71 ns 640.14 ns]
+                        change: [-11.953% -11.339% -10.710%] (p = 0.00 < 0.05)
                         Performance has improved.
 Found 11 outliers among 100 measurements (11.00%)
+  6 (6.00%) high mild
+  5 (5.00%) high severe
+
+Int %                   time:   [716.16 ns 730.71 ns 746.32 ns]
+                        change: [-7.4528% -4.2331% -0.8918%] (p = 0.02 < 0.05)
+                        Change within noise threshold.
+Found 6 outliers among 100 measurements (6.00%)
   3 (3.00%) high mild
-  8 (8.00%) high severe
+  3 (3.00%) high severe
+
+Int gcd                 time:   [20.971 µs 21.026 µs 21.089 µs]
+                        change: [-26.840% -26.364% -25.859%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 12 outliers among 100 measurements (12.00%)
+  6 (6.00%) high mild
+  6 (6.00%) high severe
 */
